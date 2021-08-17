96542283405cf33740d37e265306a4c58e918c81
Shubham Sharma <sharma.shubham0191@gmail.com>
2021-08-17 22:26:13 +0530

    added automated-text

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    strings: ["Espresso", "Cappuccino", "Iced Latte", "Decaf", "Iced Coffee"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
