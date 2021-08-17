3efb28cbc7e5bf5e6c88ee0b78b948449fabf113
Shubham Sharma <sharma.shubham0191@gmail.com>
2021-08-17 23:43:58 +0530

    top section created

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-    strings: ["Espresso", "Cappuccino", "Iced Latte", "Decaf", "Iced Coffee"],
+    strings: ["Espresso.", "Cappuccino.", "Iced Latte.", "Decaf.", "Iced Coffee."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
