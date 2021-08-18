e4820cd93612e5d7d259055ca19e2723d4ef9cb7
Shubham Sharma <sharma.shubham0191@gmail.com>
2021-08-18 23:49:46 +0530

    madal window completed

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,19 @@
-var typed = new Typed(".typing", {
+// Automate-typing
+const typed = new Typed(".typing", {
     strings: ["Espresso.", "Cappuccino.", "Iced Latte.", "Decaf.", "Iced Coffee."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 });
 
+
 // Modal Content
-var modal = document.getElementById('modal');
-var span = document.getElementsByClassName("close")[0];
+const modal = document.getElementById('modal');
+const span = document.getElementsByClassName("close")[0];
 
 setTimeout(function () {
     modal.style.display = 'block';
-}, 3000);
+}, 1000);
 
 span.onclick = function () {
     modal.style.display = "none";
